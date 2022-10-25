79541f8ac4151e2b7102e854b195496e11ba7ca7
shahtaz <shahtazrahman17@gmail.com>
2022-10-25 14:46:57 +0600

    basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import logo from './assets/icons/logo.png'
+import Navbar from './components/Navbar/Navbar';
+import Homepage from './pages/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Homepage/>
     </div>
   );
 }
