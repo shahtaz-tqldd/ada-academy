802f53ac0a231f45e7e3c2db3d2d105b7b9ef96a
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-27 12:37:24 +0600

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      All Rights Reserved to &copy; Ada Academy
+    </div>
+  )
+}
+
+export default Footer
