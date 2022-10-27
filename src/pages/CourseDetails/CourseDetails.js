97a6958ec558525ac56c2dc280efb0abf1fdd463
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-27 16:59:31 +0600

    react to pdf download added

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -2,18 +2,23 @@ import React from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
 import './CourseDetails.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faStar, faClock } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faStar, faClock, faDownload } from '@fortawesome/free-solid-svg-icons'
+import ReactPrint from 'react-to-print';
+import { useRef } from 'react';
 
 const CourseDetails = () => {
     const course = useLoaderData()
     const { id, courseImg, name, description, duration, instructor, outline, ratings } = course
     console.log(course)
+    const ref = useRef()
     return (
         <div className='container course-details'>
             <Link to="/courses"><div><FontAwesomeIcon icon={faArrowLeft} /> Go Back</div></Link>
+            <div ref={ref}>
             <img src={courseImg} alt={name}></img>
-            <h1 className='title'>{name}</h1>
+            <h1 className='title'>{name}</h1> 
             <p className='time'><FontAwesomeIcon icon={faClock}/> Duration: {duration} <span><FontAwesomeIcon icon={faStar} /> {ratings} </span></p>
+            <ReactPrint trigger={()=> <div className='download'><FontAwesomeIcon icon={faDownload}/> Download</div>} content={()=> ref.current} ></ReactPrint>
             
             <h2>Course Description</h2>
             <p className='description'>{description}</p>
@@ -29,8 +34,8 @@ const CourseDetails = () => {
                     </div>
                 </div>
             </div>
+            </div>
             <Link to={`/courses/${id}/premium-access`}><button>Get Premium Access</button></Link>
-
         </div>
     )
 }
